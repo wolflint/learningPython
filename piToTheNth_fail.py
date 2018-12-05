f98ebed0a2463515a5699a4f177ccaec96737267
wolflint <wolflint@github.com>
2018-12-05 10:40:47 +0000

    Failed piToTheNth program

---
+++ piToTheNth_fail.py
@@ -0,0 +1,34 @@
+#import requirements
+import math, decimal
+
+# value of pi
+piInt = 3
+piDec = 14159265358979323846264338327950288419716939937510
+
+# defining round down function
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
+
+# user input validation for interger
+def inputNumber(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except:
+            print("I need an interger! Try again...")
+            continue
+        else:
+            return userInput
+            break
+
+# prompt for user input and store in variable
+piLength = inputNumber("To which decimal place should Pi be displayed? ")
+
+# check that piLength is within range
+while piLength not in range(-1, 51):
+    print("Error. You've selected a number out of range.\nPlease choose a number between 0 and 50")
+    piLength = inputNumber("To which decimal place should Pi be displayed? ")
+
+decimal.getcontext().prec = (piLength + 2)
+print(decimal.Decimal(pi))
