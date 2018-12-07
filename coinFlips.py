81949eb16a7cb253b579fcac479be79b731d38b0
Marcin Wolf <marcinwolf363@gmail.com>
2018-12-07 16:44:54 +0000

    Changes to functionality
    Added counter and split code into methods

--- coinFlips.py
+++ coinFlips.py
@@ -11,24 +11,24 @@ def inputNumber(message):
             continue
         else:
             return userInput
-            break
 
-# user input
-multiple = inputNumber("Please enter the amount of times you'd like to flip the coin: ")
-
-# user input validation
-# while inputNumber not in range(0, 99):
-#     print("Invalid input. Please type in a number between 1 and 99")
-#     inputNumber = int(input("How many times should the coin be flipped? "))
-# check if multiplier argument has any input data
-if multiple > 0:
-    for value in range(0, multiple):
+def tossCoins (amount):
+    heads = 0
+    tails = 0
+    for i in range(amount):
         # generate variable value 0 or 1 randomly
         toss = random.randint(0, 1)
-
-        # if variable value is 0, display HEADS
+            # if variable value is 0, display HEADS
         if toss == 0:
-            print("HEADS")
+            print("HEADS\n")
+            heads+=1
         # else if variable value is 1, display TAILS
         elif toss == 1:
-            print("TAILS")
+            print("TAILS\n")
+            tails+=1
+    return [heads, tails]
+
+# user input
+amount = inputNumber("Please enter the amount of times you'd like to flip the coin: ")
+results = tossCoins(amount)
+print("Heads: " + str(results[0]) + " Tails: " + str(results[1]))
